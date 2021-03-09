69740543b95e2cf82e0748631f49f8579563ab9c
Aman Priyadarshi <55681932+aman-pri@users.noreply.github.com>
2021-03-09 14:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function show1()
+{
+  document.getElementById("ren1").opacity=100;
+  document.getElementById("ren2").opacity=0;
+  document.getElementById("ren3").opacity=0;
+}
+
+function show2()
+{
+  document.getElementById("ren1").opacity=0;
+  document.getElementById("ren2").opacity=100;
+  document.getElementById("ren3").opacity=0;
+}
+
+function show3()
+{
+  document.getElementById("ren1").opacity=0;
+  document.getElementById("ren2").opacity=0;
+  document.getElementById("ren3").opacity=100;
+}
